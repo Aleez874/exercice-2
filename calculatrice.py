ad5743b4248265232edbd0d97d51bec6e9305eb0
aleezay <aleezay.asif@student.arperuwelz.be>
2024-05-29 13:55:28 +0200

    Deuxième commit - ajout de calculatrice.py

---
+++ calculatrice.py
@@ -0,0 +1,11 @@
+def addition (n1, n2):
+    return n1 + n2
+print(addition(5, 10))
+
+def soustraction (n1, n2):
+    return n1 - n2
+print(soustraction(5, 10))
+
+def multiplication (n1, n2):
+    return n1 * n2
+print(multiplication(5, 10))
